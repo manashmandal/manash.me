0aa7ff803c57ee0798225a6379d1fa3731d3c70a
Manash Kumar Mandal <manashmndl@gmail.com>
2016-08-02 21:29:55 +0600

    added buch

--- js/me.js
+++ js/me.js
@@ -1,12 +1,13 @@
-$(document).ready(function () {
-    $('.nav li a').click(function(e) {
+// $(document).ready(function () {
+//     $('.nav li a').click(function(e) {
+//
+//         $('.nav li').removeClass('active');
+//
+//         var $parent = $(this).parent();
+//         if (!$parent.hasClass('active')) {
+//             $parent.addClass('active');
+//         }
+//         e.preventDefault();
+//     });
+// });
 
-        $('.nav li').removeClass('active');
-
-        var $parent = $(this).parent();
-        if (!$parent.hasClass('active')) {
-            $parent.addClass('active');
-        }
-        e.preventDefault();
-    });
-});
